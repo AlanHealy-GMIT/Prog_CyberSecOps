db7690c604b65d8ebfb1f580dea1b5e85b573cc3
AlanHealy-GMIT <G00398793@gmit.ie>
2021-02-27 23:53:03 +0000

    week 4 labs missed

---
+++ week04-flow/evens.py
@@ -0,0 +1,11 @@
+# evens.py
+# This program prints out even numbers in a range
+# Author: Alan Healy
+# Date Created: 27-FEB-2021
+
+numberTo = 100
+evenNum  = 2
+
+while evenNum <= numberTo:
+    print (evenNum)
+    evenNum +=2
