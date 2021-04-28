569f4f44ff662ce92f6e27da120dbcd39b546afa
AlanHealy-GMIT <G00398793@gmit.ie>
2021-04-28 11:25:58 +0100

    week10 lab files

---
+++ week10-errors/useFib.py
@@ -0,0 +1,11 @@
+# useFib.py
+# This program tries different tests and errors.
+# Author: Alan Healy
+# Date Created: 28-APR-2021
+
+# This program prompts the user for a number and
+# Prints out the fibonacci sequence of that many numbers
+import myFunctions
+
+nTimes = int(input('How many: '))
+print(myFunctions.fibonacci(nTimes))
